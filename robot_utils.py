18db0f77e8382e41122093eb5af85fcb3027a945
chr0torp <christian.torp@hotmail.com>
2025-06-23 18:48:31 +0200

    safe

--- robot_utils.py
+++ robot_utils.py
@@ -3,3 +3,9 @@
 
 def wait_for_key(prompt="Press Enter to continue..."):
     input(prompt)
+
+
+def safe_pos(dest):
+    """
+    """
+    return [dest[0], dest[1], dest[2] + 0.1, dest[3], dest[4], dest[5]]
