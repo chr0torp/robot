37b4157820b44b24fdd627a283dc40ff18dc2e00
chr0torp <christian.torp@hotmail.com>
2025-07-10 13:17:59 +0200

    aa

--- robot_utils.py
+++ robot_utils.py
@@ -84,5 +84,30 @@ def find_height(mid, rtde_c, target_x1, target_y1, Z_HEIGHT, FIXED_ORIENTATION, 
 
     return Z_HEIGHT, True
 
+def adjust_pos(needle_pos, mid_n, target_x1, target_y1, Z_HEIGHT, FIXED_ORIENTATION, rtde_c, SPEED, ACCELERATION):
+    """
+    Adjusts the needle position based on its current position relative to the midpoint.
+    """
+    last_pos = needle_pos
+    if needle_pos > (mid_n + 25):
+        if needle_pos < (mid_n + 150):
+            target_y1 -= 0.0025
+        else:
+            target_y1 -= 0.01
 
+        target_pose1 = [target_x1, target_y1, Z_HEIGHT] + FIXED_ORIENTATION
+        rtde_c.moveL(target_pose1, SPEED, ACCELERATION)
+        stop_move(rtde_c)
+
+    elif needle_pos < (mid_n - 25):
+        if needle_pos > (mid_n - 150):
+            target_y1 += 0.0025
+        else:
+            target_y1 += 0.01
+
+        target_pose1 = [target_x1, target_y1, Z_HEIGHT] + FIXED_ORIENTATION
+        rtde_c.moveL(target_pose1, SPEED, ACCELERATION)
+        stop_move(rtde_c)
+    
+    return target_y1, last_pos
 
