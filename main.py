bc33c37cc28c51b25bd969de42a2469d20b6ebe5
chr0torp <christian.torp@hotmail.com>
2025-07-10 13:45:20 +0200

    aa

--- main.py
+++ main.py
@@ -82,10 +82,15 @@ try:
     Z_HEIGHT, bo = find_height(safe_mid, rtde_c, target_x1, target_y1, Z_HEIGHT, FIXED_ORIENTATION, SPEED, ACCELERATION)
     print(f"Final Z Height after adjustment: {Z_HEIGHT}")
 
-    if not bo:
-        print("No valid height detected, exiting.")
-        rtde_c.disconnect()
-        exit(1)
+    while not bo:
+        target_y1 += 0.02
+
+        target_pose1 = [target_x1, target_y1, Z_HEIGHT] + FIXED_ORIENTATION
+        rtde_c.moveL(target_pose1, SPEED, ACCELERATION)
+        stop_move(rtde_c)
+
+        Z_HEIGHT, bo = find_height(safe_mid, rtde_c, target_x1, target_y1, Z_HEIGHT, FIXED_ORIENTATION, SPEED, ACCELERATION)
+
 
     if bo:
 
